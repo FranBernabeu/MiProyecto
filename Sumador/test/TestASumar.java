a9a5a6b22d555719cf9299217aa2f37467bae06a
FranBernabeu <Usuario@WIN-M8MEG52P4DJ>
2019-03-04 09:41:45 +0100

    Segunda prueba

--- Sumador/test/TestASumar.java
+++ Sumador/test/TestASumar.java
@@ -10,4 +10,12 @@ class TestASumar {
         String resultado = sumi.mostrar();
         assertEquals("5 = 5",resultado );
     }
+	
+		@Test
+	    public void testNegativo() {
+	        ASumar sumi = new ASumar("-5");
+	        String resultado = sumi.mostrar();
+	        assertEquals("Es un número negativo",resultado );
+	    }
+		
 }
