7f2888b4e5340d7b19629137f626b9d58edc6674
FranBernabeu <Usuario@WIN-M8MEG52P4DJ>
2019-03-04 09:43:30 +0100

    Tercera Prueba

--- Sumador/test/TestASumar.java
+++ Sumador/test/TestASumar.java
@@ -5,17 +5,30 @@ import org.junit.jupiter.api.Test;
 class TestASumar {
 
 	@Test
-    public void testUnaCifra() {
-        ASumar sumi = new ASumar("5");
-        String resultado = sumi.mostrar();
-        assertEquals("5 = 5",resultado );
-    }
-	
-		@Test
-	    public void testNegativo() {
-	        ASumar sumi = new ASumar("-5");
-	        String resultado = sumi.mostrar();
-	        assertEquals("Es un número negativo",resultado );
-	    }
-		
+	public void testUnaCifra() {
+		ASumar sumi = new ASumar("5");
+		String resultado = sumi.mostrar();
+		assertEquals("5 = 5", resultado);
+	}
+
+	@Test
+	public void testNegativo() {
+		ASumar sumi = new ASumar("-5");
+		String resultado = sumi.mostrar();
+		assertEquals("Es un número negativo", resultado);
+	}
+
+	@Test
+	public void testTotal() {
+		ASumar sumi = new ASumar("3433");
+		int resultado = sumi.total();
+		assertEquals(13, resultado);
+	}
+
+	@Test
+	public void testVariasCifra() {
+		ASumar sumi = new ASumar("3433");
+		String resultado = sumi.mostrar();
+		assertEquals("3 + 4 + 3 + 3 = 13", resultado);
+	}
 }
